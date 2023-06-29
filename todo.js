9343a267525bc9a8d431963890c07287f218dd2a
Pranjal Sharma <72206608+Pranjal23a@users.noreply.github.com>
2023-06-29 19:09:14 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,15 +1,49 @@
-const tasks = [];
+let tasks = [];
 const taskList = document.getElementById("list");
 const addTaskInput = document.getElementById("add");
 const tasksCounter = document.getElementById("tasks-counter");
 
 console.log("Working");
 
-function renderList() {}
+function addTaskToDOM(task) {
+  const li = document.createElement("li");
+  li.innerHTML = `<input type="checkbox" id="${task.id}" ${task.done ? 'checked' : ''} data-id="${task.id}" class="custom-checkbox">
+  <label for="${task.id}">${task.text}</label>
+  <img src="bin.svg" class="delete" data-id="${task.id}" />`;
 
-function markTaskAsComplete(taskId) {}
+  taskList.append(li);
+}
 
-function deleteTask(taskId) {}
+function renderList() {
+  taskList.innerHTML = "";
+  for (let i = 0; i < tasks.length; i++) {
+    addTaskToDOM(tasks[i]);
+  }
+}
+
+function toggleTask(taskId) {
+  const newTasks1 = tasks.filter(function (task) {
+    return task.id === taskId;
+  });
+  if (newTasks1.length > 0) {
+    const currentTask = newTasks1[0];
+    currentTask.done = !currentTask.done;
+    tasks = newTasks1;
+    renderList();
+    showNotification("Task Completed Successfully!!");
+    return;
+  }
+  showNotification("Could Not Toggele The Task");
+}
+
+function deleteTask(taskId) {
+  const newTasks = tasks.filter(function (task) {
+    return task.id != taskId;
+  });
+  tasks = newTasks;
+  renderList();
+  showNotification("Task Deleted Successfully!!");
+}
 
 function addTask(task) {
   if (task) {
@@ -44,4 +78,24 @@ function handleInputKeypress(e) {
   }
 }
 
-addTaskInput.addEventListener("keyup", handleInputKeypress);
+function handleClickListner(e) {
+  const target = e.target;
+  if (target.className === "custom-checkbox") {
+    const taskId = target.id;
+    toggleTask(taskId);
+    return;
+  }
+  else if(target.className === "delete")
+  {
+    const taskId= target.dataset.id;
+    deleteTask(taskId);
+    return;
+  }
+}
+
+function initializeAPP() {
+  addTaskInput.addEventListener("keyup", handleInputKeypress);
+  document.addEventListener("click", handleClickListner);
+}
+
+initializeAPP();
